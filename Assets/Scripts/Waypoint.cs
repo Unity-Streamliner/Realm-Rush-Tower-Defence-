f6d847c1fb9068cc3458138d3d4a4e1633264aa1
tiger <thisisstreamliner@gmail.com>
2022-12-18 00:39:33 +0200

    Targeting Enemies

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject tower;
     void OnMouseDown() {
         if (isPlaceable) {
             print(transform.name);
+            Instantiate(tower, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
     }
 }
