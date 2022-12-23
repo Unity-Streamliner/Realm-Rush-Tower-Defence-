a8f7aaa0877dd610fd61b291146f630fb3426577
tiger <thisisstreamliner@gmail.com>
2022-12-23 18:16:15 +0200

    Add UI

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 250;
     [SerializeField] int currentBalance;
+    [SerializeField] TMP_Text displayBalance;
 
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplayBalance();
+    }
+
+    void UpdateDisplayBalance()
+    {
+        displayBalance.text = $"Gold: {currentBalance}";
     }
 
     public int CurrentBalance { get { return currentBalance; } }
@@ -18,11 +26,13 @@ public class Bank : MonoBehaviour
     public void Deposite(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplayBalance();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplayBalance();
 
         if (currentBalance < 0)
         {
