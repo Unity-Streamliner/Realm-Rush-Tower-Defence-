51c18c977da54bd4dd267fab334edc3cd2d163a2
tiger <thisisstreamliner@gmail.com>
2023-01-04 13:08:50 +0200

    Set Colour For Nodes

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -7,14 +7,16 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -54,11 +56,25 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor() 
     {
-        if (waypoint.IsPlaceable) 
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+        
+        if (!node.isWalkable)
+        {
+            label.color = blockedColor;
+        }
+        else if (node.isPath) {
+            label.color = pathColor;
+        }
+        else if (node.isExplored) {
+            label.color = exploredColor;
+        }
+        else 
         {
             label.color = defaultColor;
-        } else {
-            label.color = blockedColor;
         }
     }
 
