f38f3d6e72f35851364eb5509e3cbac09d8603f3
tiger <thisisstreamliner@gmail.com>
2023-01-10 23:53:44 +0200

    refactor Tiles Refactor GridManager

--- Assets/Scripts/Pathfinding/GridManager.cs
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
+    [Tooltip("World Grid Size - Should match UnityEditor snap settings.")]
+    [SerializeField] int unityGridSize = 10;
+    public int UnityGridSize { get { return unityGridSize; } }
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
     public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
 
@@ -20,6 +23,23 @@ public class GridManager : MonoBehaviour
             return grid[coordinates];
         }
         return null;
+    }
+
+    public void BlocNode(Vector2Int coordinates)
+    {
+        Node node = GetNode(coordinates);
+        if (node != null) 
+        {
+            node.isWalkable = false;
+        }
+    }
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / unityGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / unityGridSize);
+        return coordinates;
     }
 
     void CreateGrid()
